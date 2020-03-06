26afd4a6d8abd2e63335856085c6b80aedaf2363
Fernando Perez <61566161+fernandoapem@users.noreply.github.com>
2020-03-05 20:07:55 -0500

    Implement Firestore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,31 @@
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import { RouteReuseStrategy } from '@angular/router';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import{AngularFireModule} from '@angular/fire'
+import { environment } from 'src/environments/environment';
+import{AngularFirestoreModule,FirestoreSettingsToken} from '@angular/fire/firestore'
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    { provide: FirestoreSettingsToken, useValue: {}}
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ src/app/pages/idea-list/idea-list.page.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IdeaService, Idea } from 'src/app/services/idea.service';
+
+@Component({
+  selector: 'app-idea-list',
+  templateUrl: './idea-list.page.html',
+  styleUrls: ['./idea-list.page.scss'],
+})
+export class IdeaListPage implements OnInit {
+
+  private ideas: Observable<Idea[]>;
+  constructor(private ideaService: IdeaService) { }
+
+  ngOnInit() {
+    this.ideas = this.ideaService.getIdeas();
+  }
+
+}
